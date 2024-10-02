c4e4309411cc7f146d20aeb7fb57e3a2bbdf1446
FREDDYSTICAS <cfreddystiven@gmail.com>
2024-10-02 08:46:16 -0500

    Primer commit - añadir todos los archivos del proyecto

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("hello word");
+
+// variables -> var 
+var numero = 25;
+var nombre = 'freddy castro';
+var numero2 = 'federico valverde';
+
+var suma = numero + numero;
+
+console.log(numero);
+console.log(nommbre);
+console.log(suma);
+
+comsole.log('el tipo de la variable nombre es:', typeof nombre);
+comsole.log('el tipo de la variable nombre es:', typeof numero);
+comsole.log('el tipo de la variable nombre es:', typeof numero2);
+comsole.log('el tipo de la variable nombre es:', typeof edad);
+
+// pedir datos por consola
+var telefono = parseInt(prompt("Ingresa el telefono movil :"));
+console.log(telefono);
+
+// condiciones
+var numero3 = 7;
+suma
